a17233c267891cd04f5b6698ba9e5a216ec183ed
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2025-04-13 14:56:56 -0400

    chore(frontend): agregar configuración para nativewind

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import './global.css';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text className="font-bold text-red-500 text-xl">Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
