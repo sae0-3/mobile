746c2c1ea8f033b3f279fd4dcdd26cb359f8abcd
efrain <efrainperalta.f@gmail.com>
2025-04-19 10:50:26 -0400

    feat(base de datos): definir las tablas user_address, branches, orders y order_details

--- database/structure/init.sql
+++ database/structure/init.sql
@@ -1,13 +1,24 @@
 create type user_role as enum ('client', 'dealer', 'admin');
 create type auth_provider_type as enum ('local', 'google');
+create type order_status as enum('pending', 'in_progress', 'delivered', 'cancelled');
 
 create table users (
     id uuid primary key default gen_random_uuid(),
     email text unique not null,
     name text,
     role user_role not null default 'client',
+	phone varchar(8),
     created_at timestamp not null default now(),
     updated_at timestamp not null default now()
+);
+
+create table user_address (
+	id uuid primary key default gen_random_uuid(),
+	user_id uuid not null references users(id) on delete cascade,
+	address text not null,
+	latitud decimal(9,6),
+	longitud decimal(9,6),
+	created_at timestamp default now()
 );
 
 create table auth_providers (
@@ -19,6 +30,15 @@ create table auth_providers (
     created_at timestamp not null default now(),
     updated_at timestamp not null default now(),
     unique(user_id, provider)
+);
+
+create table branches (
+	id uuid primary key default gen_random_uuid(),
+	name varchar(100) not null,
+	address text not null,
+	phone varchar(8),
+	open_hour time,
+	close_hour time
 );
 
 create table categories (
@@ -49,3 +69,24 @@ create table product_categories (
     category_id uuid not null references categories(id) on delete cascade,
     primary key (product_id, category_id)
 );
+
+create table orders (
+    id uuid primary key default gen_random_uuid(),
+    user_id uuid not null references users(id) on delete set null,
+    branch_id uuid not null references branches(id),
+    delivery_id uuid references users(id),
+    status order_status not null default 'pending',
+    created_at timestamp not null default now(),
+    total numeric(10,2) not null,
+    delivery_address text not null
+);
+
+create table order_details (
+    id uuid primary key default gen_random_uuid(),
+    order_id uuid not null references orders(id) on delete cascade,
+    product_id uuid not null references products(id),
+    quantity int not null,
+    unit_price numeric(10,2) not null,
+    subtotal numeric(10,2) not null
+);
+
