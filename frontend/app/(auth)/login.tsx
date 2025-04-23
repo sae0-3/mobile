514c060e518824801b0964c3d7051f6a26d5346a
efrain <efrainperalta.f@gmail.com>
2025-04-23 19:13:34 -0400

    feat(frontend): ver contraseña y statusBar

--- frontend/app/(auth)/login.tsx
+++ frontend/app/(auth)/login.tsx
@@ -1,6 +1,7 @@
 import { Link, useRouter } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { Button, Text, TextInput, View } from 'react-native';
+import { ActivityIndicator, KeyboardAvoidingView, Platform, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { Feather } from '@expo/vector-icons'
 import { useLogin } from '../../src/hooks/useAuth';
 import { useAuth } from '../../src/stores/auth';
 import { Logo } from '../../assets/Logo';
@@ -8,6 +9,7 @@ import { Logo } from '../../assets/Logo';
 export default function LoginScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
   const router = useRouter();
   const { isAuthenticated, login, role } = useAuth();
   const { mutate, isPending, error } = useLogin();
@@ -19,6 +21,7 @@ export default function LoginScreen() {
   }, [isAuthenticated, role]);
 
   const handleLogin = () => {
+    if (!email || !password) return;
     mutate({ email, password }, {
       onSuccess: (response) => {
         const token = response.data.access_token;
@@ -28,41 +31,69 @@ export default function LoginScreen() {
   };
 
   return (
-    <View className='flex-1 justify-center'>
-      <View className="items-center justify-center mb-10">
-        <Logo width="180" height="180" />
-      </View>
+    <KeyboardAvoidingView className="flex-1" enabled={true} behavior={Platform.OS === "ios" ? "padding" : "height"}>
+      <View className='flex-1 justify-center '>
+        <View className="items-center justify-center mb-10 mt-10">
+          <Logo width="180" height="180" />
+        </View>
 
-      <View className="w-10/12 mx-auto flex gap-4">
-        <Text className="text-2xl font-bold mb-6 text-center">Iniciar Sesión</Text>
-        <TextInput
-          className="h-16 border border-gray-300 rounded-lg px-3"
-          placeholder="Correo electrónico"
-          value={email}
-          onChangeText={setEmail}
-          autoCapitalize="none"
-          keyboardType="email-address"
-        />
-        <TextInput
-          className="h-16 border border-gray-300 rounded-lg px-3"
-          placeholder="Contraseña"
-          value={password}
-          onChangeText={setPassword}
-          secureTextEntry
-        />
+        <View className="w-10/12 mx-auto flex gap-4">
+          <Text className="text-2xl font-bold mb-6 text-center">Iniciar Sesión</Text>
+          <TextInput
+            className="h-16 border border-gray-300 rounded-lg px-3"
+            placeholder="Correo electrónico"
+            value={email}
+            onChangeText={setEmail}
+            autoCapitalize="none"
+            keyboardType="email-address"
+            selectionColor="#f2c558"
+            textContentType="emailAddress"
+            importantForAutofill="yes"
+          />
 
-        <Button title={isPending ? 'Cargando...' : 'Ingresar'} onPress={handleLogin} disabled={isPending} />
+          <View className="relative mb-4">
+            <TextInput
+              className="h-16 border border-gray-300 rounded-lg px-3"
+              placeholder="Contraseña"
+              value={password}
+              onChangeText={setPassword}
+              secureTextEntry={!showPassword}
+              selectionColor="#f2c558"
+              textContentType="password"
+              importantForAutofill="yes"
+              autoCapitalize="none"
+            />
+            <TouchableOpacity
+              onPress={() => setShowPassword(!showPassword)}
+              className="absolute right-4 top-5 "
+            >
+              <Feather name={showPassword ? "eye-off" : "eye"} size={24} color="#9ca3af" />
+            </TouchableOpacity>
+          </View>
 
-        {error && (
-          <Text className="text-red-500 text-center">
-            {error.response?.data.message || "Error al iniciar sesión"}
+          <TouchableOpacity
+            onPress={handleLogin}
+            disabled={isPending}
+            className={`h-12 rounded-lg justify-center items-center ${isPending ? 'bg-gray-300' : 'bg-[#f2c558]'}`}
+          >
+            {isPending ? (
+              <ActivityIndicator size="small" color="#fff" />
+            ) : (
+              <Text className="text-white font-semibold text-base">Ingresar</Text>
+            )}
+          </TouchableOpacity>
+
+          {error && (
+            <Text className="text-red-500 text-center">
+              {error.response?.data.message || "Error al iniciar sesión"}
+            </Text>
+          )}
+
+          <Text className="text-center">
+            ¿No tienes una cuenta? <Link href="/register" className="text-cyan-600">Regístrate</Link>
           </Text>
-        )}
-
-        <Text className="text-center">
-          ¿No tienes una cuenta? <Link href="/register" className="text-cyan-600">Regístrate</Link>
-        </Text>
+        </View>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -2,10 +2,12 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
 import { queryClient } from '../src/lib/queryClient';
 import '../global.css';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
+      <StatusBar translucent={true} />
       <Stack
         screenOptions={{
           headerShown: false,
