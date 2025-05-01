7af01184b8eb807d9f3bed718fc6ee1b8a150594
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2025-05-01 15:22:16 -0400

    refactor(database): separar la tabla users en clients, admins, dealers

--- database/structure/init.sql
+++ database/structure/init.sql
@@ -1,24 +1,39 @@
-create type user_role as enum ('client', 'dealer', 'admin');
+-- create type user_role as enum ('client', 'dealer', 'admin');
 create type auth_provider_type as enum ('local', 'google');
-create type order_status as enum('pending', 'in_progress', 'delivered', 'cancelled');
+create type order_status as enum ('pending', 'in_progress', 'delivered', 'cancelled');
+create type vehicle_type as enum ('motorcycle', 'bicycle', 'car');
 
 create table users (
     id uuid primary key default gen_random_uuid(),
     email text unique not null,
-    name text,
-    role user_role not null default 'client',
-	phone varchar(8),
+--    role user_role not null default 'client',
     created_at timestamp not null default now(),
     updated_at timestamp not null default now()
 );
 
+create table admins (
+    user_id uuid primary key references users(id) on delete cascade
+);
+
+create table dealers (
+    user_id uuid primary key references users(id) on delete cascade,
+    name text not null,
+    vehicle vehicle_type not null
+);
+
+create table clients (
+    user_id uuid primary key references users(id) on delete cascade,
+    name text not null,
+    phone varchar(8)
+);
+
 create table user_address (
-	id uuid primary key default gen_random_uuid(),
-	user_id uuid not null references users(id) on delete cascade,
-	address text not null,
-	latitud decimal(9,6),
-	longitud decimal(9,6),
-	created_at timestamp default now()
+    id uuid primary key default gen_random_uuid(),
+    user_id uuid not null references users(id) on delete cascade,
+    address text not null,
+    latitud decimal(9,6),
+    longitud decimal(9,6),
+    created_at timestamp default now()
 );
 
 create table auth_providers (
@@ -33,12 +48,12 @@ create table auth_providers (
 );
 
 create table branches (
-	id uuid primary key default gen_random_uuid(),
-	name varchar(100) not null,
-	address text not null,
-	phone varchar(8),
-	open_hour time,
-	close_hour time
+    id uuid primary key default gen_random_uuid(),
+    name varchar(100) not null,
+    address text not null,
+    phone varchar(8),
+    open_hour time,
+    close_hour time
 );
 
 create table categories (
@@ -55,7 +70,7 @@ create table products (
     name text not null,
     description text,
     img_reference text,
-    price numeric(5, 2) not null,
+    price numeric(10, 2) not null,
     available boolean not null default true,
     ingredients json,
     created_at timestamp not null default now(),
@@ -89,4 +104,23 @@ create table order_details (
     unit_price numeric(10,2) not null,
     subtotal numeric(10,2) not null
 );
+
+
+
+-- ADMIN
+with new_user as (
+    insert into users (email)
+    values ('admin@gmail.com')
+    returning id
+),
+new_admin as (
+    insert into admins (user_id)
+    select id from new_user
+),
+new_auth as (
+    insert into auth_providers (user_id, provider, password_hash)
+    select id, 'local', '$2b$10$6OaPiCQw1l9VhwMyQB2HSuw0UuOGF7JCbkLRprwKO7x.pF4Xv27Im' -- Contra_1234
+    from new_user
+)
+select * from new_user;
 
