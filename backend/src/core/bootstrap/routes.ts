f9a906f307dd409793a50b1b24963331045eb290
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2025-05-10 16:12:11 -0400

    feat(backend): agregar endpoint para users

--- backend/src/core/bootstrap/routes.ts
+++ backend/src/core/bootstrap/routes.ts
@@ -1,8 +1,10 @@
 import { Express } from 'express';
 import authRoutes from '../../modules/auth/routes/auth.routes';
 import catalogRoutes from '../../modules/catalog/catalog.routes';
+import usersRoutes from '../../modules/users/users.routes';
 
 export const registerRoutes = (app: Express) => {
   app.use('/api/auth', authRoutes);
   app.use('/api/catalog', catalogRoutes);
+  app.use('/api/users', usersRoutes);
 };
