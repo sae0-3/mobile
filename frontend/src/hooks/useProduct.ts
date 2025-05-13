80156b9c9c00fe76eb1cff690589907a9c501970
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2025-05-13 13:19:04 -0400

    feat(frontend): agregar la opción de vincular/desvincular producto-categoría

--- frontend/src/hooks/useProduct.ts
+++ frontend/src/hooks/useProduct.ts
@@ -1,9 +1,18 @@
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import { ProductUpdateDto } from '../dtos/productDto';
-import { create, getAll, getById, updateById } from '../services/productService';
+import {
+  create,
+  getAll,
+  getById,
+  getCategoriesLinkedById,
+  getCategoriesUnlinkedById,
+  linkToCategory,
+  unlinkToCategory,
+  updateById,
+} from '../services/productService';
 import { useAuth } from '../stores/auth';
-import { ProductRequest, ProductResponse, ProductsResponse } from '../types/apiTypes';
+import { ApiResponse, CategoriesResponse, ProductRequest, ProductResponse, ProductsResponse } from '../types/apiTypes';
 import { invalidateQueries } from '../utils/invalidateQueries';
 
 export const useGetAllProducts = () => {
@@ -58,5 +67,63 @@ export const useUpdateByIdProduct = (id: string) => {
     onError: (error) => {
       console.error(error.response?.data.message);
     },
+  });
+};
+
+export const useLinkProductToCategory = (product_id: string) => {
+  const { token } = useAuth();
+  const queriesToInvalidate = [
+    ['product', 'admin', product_id],
+    ['product', 'admin', product_id, 'categories', 'linked'],
+    ['product', 'admin', product_id, 'categories', 'unlinked'],
+  ];
+
+  return useMutation<ApiResponse<undefined>, AxiosError<ApiResponse<undefined>>, string>({
+    mutationFn: (category_id) => linkToCategory(product_id, category_id, token),
+    onSuccess: (data) => {
+      invalidateQueries(queriesToInvalidate);
+      console.log(data);
+    },
+    onError: (error) => {
+      console.error(error.response?.data);
+    },
+  });
+};
+
+export const useUnlinkProductToCategory = (product_id: string) => {
+  const { token } = useAuth();
+  const queriesToInvalidate = [
+    ['product', 'admin', product_id],
+    ['product', 'admin', product_id, 'categories', 'linked'],
+    ['product', 'admin', product_id, 'categories', 'unlinked'],
+  ];
+
+  return useMutation<ApiResponse<undefined>, AxiosError<ApiResponse<undefined>>, string>({
+    mutationFn: (category_id) => unlinkToCategory(product_id, category_id, token),
+    onSuccess: (data) => {
+      invalidateQueries(queriesToInvalidate);
+      console.log(data);
+    },
+    onError: (error) => {
+      console.error(error.response?.data);
+    },
+  });
+};
+
+export const useGetCategoriesLinkedByIdProduct = (id: string) => {
+  const { token } = useAuth();
+
+  return useQuery<CategoriesResponse, AxiosError<ProductResponse>>({
+    queryKey: ['product', 'admin', id, 'categories', 'linked'],
+    queryFn: () => getCategoriesLinkedById(id, token),
+  });
+};
+
+export const useGetCategoriesUnlinkedByIdProduct = (id: string) => {
+  const { token } = useAuth();
+
+  return useQuery<CategoriesResponse, AxiosError<ProductResponse>>({
+    queryKey: ['product', 'admin', id, 'categories', 'unlinked'],
+    queryFn: () => getCategoriesUnlinkedById(id, token),
   });
 };
