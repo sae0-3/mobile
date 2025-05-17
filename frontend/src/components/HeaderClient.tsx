c4f3ba586c3a711da460b0cea9aecc9d22af42d5
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2025-05-17 19:16:32 -0400

    refactor(frontend): mejorar el header del cliente

--- frontend/src/components/HeaderClient.tsx
+++ frontend/src/components/HeaderClient.tsx
@@ -1,31 +1,36 @@
-import { Link } from 'expo-router';
-import { View } from 'react-native';
+import { useRouter } from 'expo-router';
+import { TouchableOpacity, View } from 'react-native';
+import { useCartStore } from '../stores/order';
 import { Icon } from './Icon';
 
 export const HeaderClient = () => {
   const user_id = 'USER_ID';
+  const router = useRouter();
+  const { items } = useCartStore();
 
   return (
     <View className="flex-row gap-4">
-      <Link href="/client/orders">
-        <View className='w-10 h-10 items-center justify-center'>
-          <Icon
-            name="shopping-cart"
-            type="MaterialIcons"
-            size={32}
-          />
-        </View>
-      </Link>
+      <TouchableOpacity
+        onPress={() => router.push("/client/orders")}
+        className="w-10 h-10 items-center justify-center"
+      >
+        <Icon
+          name={`${items.length === 0 ? 'shopping-cart' : 'shopping-cart-checkout'}`}
+          type="MaterialIcons"
+          size={32}
+        />
+      </TouchableOpacity>
 
-      <Link href={`/client/profile/${user_id}`}>
-        <View className='w-10 h-10 items-center justify-center'>
-          <Icon
-            name="user"
-            type="FontAwesome"
-            size={32}
-          />
-        </View>
-      </Link>
+      <TouchableOpacity
+        onPress={() => router.push(`/client/profile/${user_id}`)}
+        className="w-10 h-10 items-center justify-center"
+      >
+        <Icon
+          name="user"
+          type="FontAwesome"
+          size={32}
+        />
+      </TouchableOpacity>
     </View>
   );
 };
