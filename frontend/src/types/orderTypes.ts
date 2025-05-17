ee867d0ee16038fe169d1b4201e335877acfc2b1
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2025-05-17 16:19:19 -0400

    feat(frontend): agregar store para el carrito de compras

---
+++ frontend/src/types/orderTypes.ts
@@ -0,0 +1,22 @@
+export type Product = {
+  id: string;
+  name: string;
+  img_reference: string;
+  price: number;
+};
+
+export type CartItem = {
+  product: Product;
+  quantity: number;
+};
+
+export type CartState = {
+  items: CartItem[];
+
+  addItem: (item: Omit<CartItem, 'quantity'>, quantity?: number) => void;
+  removeItem: (productId: string) => void;
+  updateQuantity: (productId: string, quantity: number) => void;
+  clearCart: () => void;
+  getSubtotal: (productId: string) => number;
+  getTotal: () => number;
+};
