f9a2d0ffc76d18721a6f8eb5f1ba2d6ebbd7d435
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2025-05-24 15:50:40 -0400

    fix(backend): listar pedidos mas recientes primero

--- backend/src/modules/orders/repositories/admin-order.repository.ts
+++ backend/src/modules/orders/repositories/admin-order.repository.ts
@@ -3,7 +3,10 @@ import { Order } from '../types/order.type';
 
 export class AdminOrderRepository extends BaseRepository {
   async findAll(): Promise<Order[]> {
-    const sql = `SELECT * FROM orders`;
+    const sql = `
+      SELECT * FROM orders
+      ORDER BY created_at DESC
+    `;
 
     return await this.query<Order>(sql, []);
   }
