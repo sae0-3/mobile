65bc9f000224122d996563f54eb0406170349fc1
Eddy Sejas Arze <eddy.sejas.arze.03@gmail.com>
2025-05-24 21:29:22 -0400

    fix(backend): ordernar por fecha de actualización

--- backend/src/modules/orders/repositories/admin-order.repository.ts
+++ backend/src/modules/orders/repositories/admin-order.repository.ts
@@ -5,7 +5,7 @@ export class AdminOrderRepository extends BaseRepository {
   async findAll(): Promise<Order[]> {
     const sql = `
       SELECT * FROM orders
-      ORDER BY created_at DESC
+      ORDER BY updated_at DESC
     `;
 
     return await this.query<Order>(sql, []);
